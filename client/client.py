bfed5613075fc9cc39bf72a6b75f6bb0e36f1c48
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2024-02-04 02:24:21 +0300

    add sockets

--- client/client.py
+++ client/client.py
@@ -1,9 +1,11 @@
 import PyQt5
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QObject, pyqtSignal, QTimer
 from client_ui import Ui_MainWindow
+import os
 
 import sys
-
+import socket
 
 class Window(QMainWindow, Ui_MainWindow):
 	def __init__(self):
@@ -12,19 +14,42 @@ class Window(QMainWindow, Ui_MainWindow):
 		self.send.clicked.connect(self.send_message)
 
 	def send_message(self):
-		message = self.message.toPlainText()
-		print("message: ", message)
+		message = self.enter.toPlainText()
+		self.sock.send(message.encode())
 
+class Chat(QObject):
+	messageReceived = pyqtSignal(str)
+
+	def __init__(self, sock):
+		super().__init__()
+		self.sock = sock
+		QTimer.singleShot(100, self.check_message)
+
+	def check_message(self):
+		try:
+			message = self.sock.recv(2048).decode()
+		except Exception:
+			QTimer.singleShot(100, self.check_message)
+			return
+		if len(message) > 0:
+			self.messageReceived.emit(message)
+			QTimer.singleShot(100, self.check_message)
 
 def application():
-	
-    app = QApplication(sys.argv)
-	
-    window = Window()
-    window.show()
 
-    sys.exit(app.exec_())
+	host = os.environ["HOST"]
+	port = os.environ["PORT"]
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	sock.connect((host, int(port)))
+	sock.setblocking(False)
+	activate_chat = Chat(sock)
 
+	app = QApplication(sys.argv)
+
+	window = Window()
+	window.show()
+
+	sys.exit(app.exec_())
 
 if __name__ == "__main__":
     application()
