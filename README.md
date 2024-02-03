c55b8bcaa9d84052cbdf56061e114b4886694aba
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2024-02-03 22:33:47 +0300

    add simple version of client

---
+++ README.md
@@ -0,0 +1,8 @@
+<h1 align="center">codic</h1>
+
+## Description
+
+<p align="center"><img src="https://github.com/baitim/codic/blob/main/images/cat.gif" width="50%"></p>
+ 
+## Support
+**This project is created by [bai_tim](https://github.com/bai_tim) and [eg-mart](https://github.com/eg-mart)**
---
+++ client/client.py
@@ -0,0 +1,30 @@
+import PyQt5
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from client_ui import Ui_MainWindow
+
+import sys
+
+
+class Window(QMainWindow, Ui_MainWindow):
+	def __init__(self):
+		super().__init__()
+		self.setupUi(self)
+		self.send.clicked.connect(self.send_message)
+
+	def send_message(self):
+		message = self.textbox.toPlainText()
+		print("message: ", message.encode())
+
+
+def application():
+	
+    app = QApplication(sys.argv)
+	
+    window = Window()
+    window.show()
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    application()
