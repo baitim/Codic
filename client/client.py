1a09d1be61f62a2b99badb1ed92d6c5520bfb19a
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2024-02-10 02:28:00 +0300

    fix bugs #2

--- client/client.py
+++ client/client.py
@@ -16,18 +16,16 @@ class Window(QMainWindow, Ui_MainWindow):
 		self.send.clicked.connect(self.send_message)
 
 	def show_message(self, message):
-		self.result.append(message)
-		print("RES ", message)
+		if message[:4] == "TIME":
+			self.result.setText(message)
 
 	def send_connect_message(self):
 		message = "CONNECT"
 		self.sock.send(message.encode())
-		print(message)
 
 	def send_message(self):
-		message = "PROGRAM " + self.message.toPlainText()
+		message = "PROGRAM 0 " + self.message.toPlainText()
 		self.sock.send(message.encode())
-		print("PROGRAM ", message)
 
 
 class Chat(QObject):
@@ -36,18 +34,18 @@ class Chat(QObject):
 	def __init__(self, sock):
 		super().__init__()
 		self.sock = sock
-		QTimer.singleShot(100, self.check_message)
+		QTimer.singleShot(1000, self.check_message)
 
 	def check_message(self):
 		try:
 			message = self.sock.recv(2048).decode()
 			print("GET ", message)
 		except Exception:
-			QTimer.singleShot(100, self.check_message)
+			QTimer.singleShot(1000, self.check_message)
 			return
 		if len(message) > 0:
 			self.messageReceived.emit(message)
-			QTimer.singleShot(100, self.check_message)
+			QTimer.singleShot(1000, self.check_message)
 
 def application():
 
